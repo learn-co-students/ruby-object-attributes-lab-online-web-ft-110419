af20e5de0d6791295ea1d89413552be3d6bfb7ba
Andrew H <andrew.o.hibbard@gmail.com>
2019-11-11 15:41:10 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,39 @@
+=begin 
+class Person
+ 
+  def initialize(first_name, last_name)
+    @first_name = first_name
+    @last_name = last_name
+  end
+ 
+  def name=(full_name)
+    first_name, last_name = full_name.split
+    @first_name = first_name
+    @last_name = last_name
+  end
+ 
+  def name
+    "#{@first_name} #{@last_name}".strip
+  end
+ 
+=end
+
+class Dog 
+
+  def name=(name)
+    @name = name 
+  end 
+  
+  def name
+    @name
+  end 
+  
+  def breed=(breed)
+    @breed = breed
+  end 
+  
+  def breed 
+    @breed
+  end 
+  
+end 
