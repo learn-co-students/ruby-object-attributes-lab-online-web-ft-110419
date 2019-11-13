31e6a64e11aa1535c59334da0a30e54e3442c2bf
Ethan Gustafson <ethan_gustafson@outlook.com>
2019-11-13 01:46:12 +0000

    Done.

--- lib/person.rb
+++ lib/person.rb
@@ -1,7 +1,24 @@
 class Person
 
-  attr_accessor :name
-  attr_accessor :job
+  def name=(name)
+  @name = name
+  end
 
+  def name
+    @name
+  end
+
+  def job=(job)
+  @job = job
+  end
+
+  def job
+    @job
+  end
 
 end
+
+
+#or it can be done like this:
+#attr_accessor :name
+#attr_accessor :job
