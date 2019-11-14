c67e1f99f8b785d2d77e5a9f8c94d6a71a4f4a1e
connor worthen <connorw220@protonmail.com>
2019-11-14 19:35:19 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,31 @@
+class Dog
+  def name
+    @name
+  end
+
+  def name
+    @name
+  end
+
+  def name=(name)
+    @name = name
+  end
+end
+
+fido = Dog.new
+
+class Dog
+  def breed
+    @breed
+  end
+
+  def breed
+    @breed
+  end
+
+  def breed=(breed)
+    @breed = breed
+  end
+end
+
+breed = Dog.new
